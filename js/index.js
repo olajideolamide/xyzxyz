c158c1bcb58dc4b9fee57f495727e7e84eae92d3
Olajide Olanrewaju <jidemac4@gmail.com>
2017-07-24 13:02:47 +0100

    inwoodpush

--- js/index.js
+++ js/index.js
@@ -17,117 +17,117 @@
  * under the License.
  */
 
-function onPushwooshInitialized(pushNotification) {
+ function onPushwooshInitialized(pushNotification) {
 
     //if you need push token at a later time you can always get it from Pushwoosh plugin
     pushNotification.getPushToken(
-        function(token) {
-            console.info('push token: ' + token);
-        }
-    );
+    	function(token) {
+    		console.info('push token: ' + token);
+    	}
+    	);
 
     //and HWID if you want to communicate with Pushwoosh API
     pushNotification.getPushwooshHWID(
-        function(token) {
-            console.info('Pushwoosh HWID: ' + token);
-        }
-    );
+    	function(token) {
+    		console.info('Pushwoosh HWID: ' + token);
+    	}
+    	);
 
     //settings tags
     pushNotification.setTags({
-            tagName: "tagValue",
-            intTagName: 10
-        },
-        function(status) {
-            console.info('setTags success: ' + JSON.stringify(status));
-        },
-        function(status) {
-            console.warn('setTags failed');
-        }
+    	tagName: "tagValue",
+    	intTagName: 10
+    },
+    function(status) {
+    	console.info('setTags success: ' + JSON.stringify(status));
+    },
+    function(status) {
+    	console.warn('setTags failed');
+    }
     );
 
     pushNotification.getTags(
-        function(status) {
-            console.info('getTags success: ' + JSON.stringify(status));
-        },
-        function(status) {
-            console.warn('getTags failed');
-        }
-    );
+    	function(status) {
+    		console.info('getTags success: ' + JSON.stringify(status));
+    	},
+    	function(status) {
+    		console.warn('getTags failed');
+    	}
+    	);
 
     //start geo tracking.
     //pushNotification.startLocationTracking();
 }
 
 function initPushwoosh() {
-    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+	var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
     //set push notifications handler
     document.addEventListener('push-notification',
-        function(event) {
-            var message = event.notification.message;
-            var userData = event.notification.userdata;
+    	function(event) {
+    		var message = event.notification.message;
+    		var userData = event.notification.userdata;
 
-            document.getElementById("pushMessage").innerHTML = message + "<p>";
-            document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
+    		document.getElementById("pushMessage").innerHTML = message + "<p>";
+    		document.getElementById("pushData").innerHTML = JSON.stringify(event.notification) + "<p>";
 
             //dump custom data to the console if it exists
             if (typeof(userData) != "undefined") {
-                console.warn('user data: ' + JSON.stringify(userData));
+            	console.warn('user data: ' + JSON.stringify(userData));
             }
         }
-    );
+        );
 
     //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
     pushNotification.onDeviceReady({
-        projectid: "60756016005",
-        appid: "4FC89B6D14A655.46488481",
-        serviceName: ""
+    	projectid: "folos-push-9821a",
+    	appid: "B4489-0C048",
+    	serviceName: ""
     });
 
     //register for push notifications
     pushNotification.registerDevice(
-        function(status) {
-            document.getElementById("pushToken").innerHTML = status.pushToken + "<p>";
-            onPushwooshInitialized(pushNotification);
-        },
-        function(status) {
-            alert("failed to register: " + status);
-            console.warn(JSON.stringify(['failed to register ', status]));
-        }
-    );
+    	function(status) {
+    		document.getElementById("pushToken").innerHTML = status.pushToken + "<p>";
+    		onPushwooshInitialized(pushNotification);
+    	},
+    	function(status) {
+    		alert("failed to register: " + status);
+    		console.warn(JSON.stringify(['failed to register ', status]));
+    	}
+    	);
 }
 
 
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+    	this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    	document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        initPushwoosh();
-        app.receivedEvent('deviceready');
+    	initPushwoosh();
+    	app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    	var parentElement = document.getElementById(id);
+    	var listeningElement = parentElement.querySelector('.listening');
+    	var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    	listeningElement.setAttribute('style', 'display:none;');
+    	receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+    	console.log('Received Event: ' + id);
     }
 };
