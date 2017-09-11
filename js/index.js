9a3430fb313213f426ec7ed238a7797b9e9c3d88
Olajide Olanrewaju <jidemac4@gmail.com>
2017-09-11 10:34:02 +0100

    king bulk sms

--- js/index.js
+++ js/index.js
@@ -1,4 +1,76 @@
+/*$(function (){
+	$.get( api+'regMobile=check&callback=?' )
+	.done(function(data) {
+		if(data == 'Enabled') $('.register').show(300);
+	});
+});*/
 
-var notification_url = 'https://www.orderfood.menu/orderonline/inwoodbarandgrill/util-store_apple_device_token';
+function login(){
+	$('.login').toggle();
+	var email = $('#email').val();
+	var password = $('#password').val();
+	if(email == '') {
+		alert('Email is required');
+		$('.login').toggle();
+		return;
+	}
+	if(password == '') {
+		alert('Password is required');
+		$('.login').toggle();
+		return;
+	}
+	/*$.get( api+'username='+escape(username)+'&password='+escape(password)+'&balance=true' )
+	.fail(function() {
+		alert( "Error connecting to server" );
+	  })
+	  .done(function(data) {
+		if(data == '2905' || data == '-2905'){
+			alert('Invalid username and password combination.');
+			logout();
+		} else {
+			setSession('username',username);
+			setSession('password',password);
+			$('.login').toggle();
+			window.location = 'menu.html';
+		}
+	  });*/
 
 
+		$.ajax({
+			type: 'GET',
+
+		  	dataType:'jsonp',
+
+			url: api+'?pd_m=login&email='+escape(email)+'&password='+escape(password)+'&callback=?',
+
+		 	jsonpCallback: 'jsonCallback',
+		    contentType: "application/json",
+		    dataType: 'jsonp',
+		    success: function(json) {
+		       if(json.response[0].code == '000')
+		       {
+					setSession('email',email);
+					setSession('password',password);
+					$('.login').toggle();
+					window.location = 'menu.html';
+				}
+				else
+				{
+					alert('Wrong email/password combination');
+					logout();
+					$('.login').toggle();
+				}
+		    },
+		    error: function(e) {
+		       alert('No internet connection');
+		       logout();
+		       $('.login').toggle();
+		    }
+		});
+
+
+}
+
+if(isLoggedIn() == true){
+	window.location = 'menu.html';
+}
